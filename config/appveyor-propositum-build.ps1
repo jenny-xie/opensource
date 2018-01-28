7181074bee3675a4c4b6c7fe23d7e5bddf5b7044
xeijin <xeijin@users.noreply.github.com>
2018-01-28 03:36:32 +0000

    Remove spacemacs .cache folder to try and reduce final artifact size
    - This is to try and address the issues with AppVeyor not picking up the
      artifact upon deployment to GitHub

--- config/appveyor-propositum-build.ps1
+++ config/appveyor-propositum-build.ps1
@@ -72,6 +72,8 @@ git clone https://github.com/syl20bnr/spacemacs .emacs.d 2> $null
 echo "Installing spacemacs layers..."
 cd $propositumApp
 .\emacs\bin\emacs.exe --daemon --eval "(progn (configuration-layer/update-packages) (save-buffers-kill-emacs))" # Force spacemacs to load (as daemon), install required packages, then quit
+cd $propositumHome
+Remove-Item -path .\.emacs.d\.cache -recurse -force # Remove the cache folder to decrease overall artifact size
 
 echo "Cloning & installing RAWgraphs..."
 ## rawgraphs installation ##
