cd719bf37b1c0ae6dcc25537a3b99d62ceba7c33
xeijin <xeijin@users.noreply.github.com>
2018-07-08 20:06:57 +0100

    Fix tangle issues & symlink readme.org

---
+++ script/propositum-windows.el
@@ -0,0 +1,18 @@
+(require 'org)
+;(require 'json)
+
+(let ((csv-path (quote "")))
+(with-temp-buffer
+  (org-table-import csv-path nil) ;; MEMO on `nil' arg is in the footnotes.
+  (setq LST (org-table-to-lisp))
+  ;; comment out or cut below one line if you don't have column names in CSV file.
+  (append (list (car LST)) '(hline) (cdr (org-table-to-lisp))))
+)
+
+(let ((csv-path (quote ""))
+      (tbl-name (quote "")))
+(save-excursion
+  (org-open-link-from-string (concat "[[" tbl-name "]]"))
+  (while (not (org-table-p)) (forward-line))
+  (org-table-export csv-path "orgtbl-to-csv"))
+)
--- script/propositum-windows.ps1
+++ script/propositum-windows.ps1
@@ -1,22 +1,3 @@
-(require 'org)
-;(require 'json)
-
-(let ((csv-path (quote "")))
-(with-temp-buffer
-  (org-table-import csv-path nil) ;; MEMO on `nil' arg is in the footnotes.
-  (setq LST (org-table-to-lisp))
-  ;; comment out or cut below one line if you don't have column names in CSV file.
-  (append (list (car LST)) '(hline) (cdr (org-table-to-lisp))))
-)
-
-(let ((csv-path (quote ""))
-      (tbl-name (quote "")))
-(save-excursion
-  (org-open-link-from-string (concat "[[" tbl-name "]]"))
-  (while (not (org-table-p)) (forward-line))
-  (org-table-export csv-path "orgtbl-to-csv"))
-)
-
 ### --- NOTE: If you are reading from the PS1 script you will find documentation sparse - this script is accompanied by an org-mode file used to literately generate it --- ####
 ### --- Please see https://github.com/xeijin/propositum for the accompanying README.org --- ###
 
