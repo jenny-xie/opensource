05d6f791ffc012ed1504893881e601b0029a383a
xeijin <xeijin@xeijin>
2018-08-19 16:40:43 +0100

    Fix Vars issue

--- script/propositum-windows.ps1
+++ script/propositum-windows.ps1
@@ -2,77 +2,77 @@
 ### --- this script is accompanied by an org-mode file used to literately generate it.   --- ###
 ### --- Please see https://github.com/xeijin/propositum for the accompanying README.org  --- ###
 
-cd $psScriptRoot
+  cd $psScriptRoot
 
 $testing = $false
 
-$buildPlatform = if ($env:APPVEYOR) {"appveyor"}
-elseif ($testing) {"testing"} # For debugging locally
-elseif ($env:computername -match "NDS.*") {"local-gs"} # Check for a GS NDS
-else {"local"}
+  $buildPlatform = if ($env:APPVEYOR) {"appveyor"}
+  elseif ($testing) {"testing"} # For debugging locally
+  elseif ($env:computername -match "NDS.*") {"local-gs"} # Check for a GS NDS
+  else {"local"}
 
-cd $PSScriptRoot
+  cd $PSScriptRoot
 
-$Host.UI.RawUI.BackgroundColor = ($bckgrnd = 'Black')
+  $Host.UI.RawUI.BackgroundColor = ($bckgrnd = 'Black')
 
-. ./propositum-helper-fns.ps1
+   . ./propositum-helper-fns.ps1
 
-Try
-{
-    $environmentVars = Import-CSV "vars-platform.csv"
-}
-Catch
-{
-    Throw "Check the CSV file actually exists and is formatted correctly before proceeding."
-    $error[0]|format-list -force
-}
+   Try
+   {
+       $environmentVars = Import-CSV "vars-platform.csv"
+   }
+   Catch
+   {
+       Throw "Check the CSV file actually exists and is formatted correctly before proceeding."
+       $error[0]|format-list -force
+   }
 
-$environmentVars | Select "exec", "var", $buildPlatform | ForEach-Object { if ($_.exec -eq "execute") {New-Variable $_.var (iex $_.$buildPlatform) -Force} else {New-Variable $_.var $_.$buildPlatform -Force}}
+   $environmentVars | Select "exec", "var", $buildPlatform | ForEach-Object { if ($_.exec -eq "execute") {New-Variable $_.var (iex $_.$buildPlatform) -Force} else {New-Variable $_.var $_.$buildPlatform -Force}}
 
-Try
-{
-    $otherVars = Import-CSV "vars-other.csv"
-}
-Catch
-{
-    Throw "Check the CSV file actually exists and is formatted correctly before proceeding."
-    $error[0]|format-list -force
-}
+   Try
+   {
+       $otherVars = Import-CSV "vars-other.csv"
+   }
+   Catch
+   {
+       Throw "Check the CSV file actually exists and is formatted correctly before proceeding."
+       $error[0]|format-list -force
+   }
 
-$otherVars | Select "exec", "var", "value" | ForEach-Object { if ($_.exec -eq "execute") {New-Variable $_.var (iex $_.value) -Force} else {New-Variable $_.var $_.value -Force}}
+   $otherVars | Select "exec", "var", "value" | ForEach-Object { if ($_.exec -eq "execute") {New-Variable $_.var (iex $_.value) -Force} else {New-Variable $_.var $_.value -Force}}
 
-if ($testing -and $propositumLocation) {Remove-Item ($propositumLocation+"\*") -Recurse -Force}
+  if ($testing -and $propositumLocation) {Remove-Item ($propositumLocation+"\*") -Recurse -Force}
 
-subst $drv $propositumLocation
+    subst $drv $propositumLocation
 
-$createdDirs = Path-CheckOrCreate -Paths $propositum.values -CreateDir
+    $createdDirs = Path-CheckOrCreate -Paths $propositum.values -CreateDir
 
-cd $propositum.root
+    cd $propositum.root
 
-[Net.ServicePointManager]::SecurityProtocol = "Tls12, Tls11, Tls, Ssl3"
+  [Net.ServicePointManager]::SecurityProtocol = "Tls12, Tls11, Tls, Ssl3"
 
-#[environment]::setEnvironmentVariable('SCOOP',($propositum.root),'User')
+  #[environment]::setEnvironmentVariable('SCOOP',($propositum.root),'User')
 
-iex (new-object net.webclient).downloadstring('https://get.scoop.sh')
+  iex (new-object net.webclient).downloadstring('https://get.scoop.sh')
 
-scoop bucket add extras
+  scoop bucket add extras
 
-scoop bucket add propositum 'https://github.com/xeijin/propositum-bucket.git'
+  scoop bucket add propositum 'https://github.com/xeijin/propositum-bucket.git'
 
-# If git isn't installed, install it
-if (-not (Get-Command 7z.exe)) {scoop install 7zip --global}
+  # If git isn't installed, install it
+  if (-not (Get-Command 7z.exe)) {scoop install 7zip --global}
 
-# If git isn't installed, install it
-if (-not (Get-Command git.exe)) {scoop install git --global}
+  # If git isn't installed, install it
+  if (-not (Get-Command git.exe)) {scoop install git --global}
 
-# Hash table with necessary details for the clone command
-$propositumRepo = [ordered]@{
-    user = "xeijin"
-    repo = "propositum"
-}
+  # Hash table with necessary details for the clone command
+  $propositumRepo = [ordered]@{
+      user = "xeijin"
+      repo = "propositum"
+  }
 
-# Clone the repo (if not AppVeyor as it is already cloned for us)
-if(-not $buildPlatform -eq "appveyor"){Github-CloneRepo "" $propositumRepo $propositumLocation}
+  # Clone the repo (if not AppVeyor as it is already cloned for us)
+  if(-not $buildPlatform -eq "appveyor"){Github-CloneRepo "" $propositumRepo $propositumLocation}
 
 $propositumScoop = @(
     'cmder',
@@ -115,5 +115,5 @@ if ($buildPlatform -eq "appveyor")
     iex "7z a -t7z -m0=lzma -mx=9 -mfb=64 -md=32m -ms=on propositum.7z C:\propositum"
 }
 
-if ($buildPlatform -eq "appveyor") {$deploy = $true}
-else {$deploy = $false}
+  if ($buildPlatform -eq "appveyor") {$deploy = $true}
+  else {$deploy = $false}
