9b0934310a38855b4688989f08e1d7b1d91ebbf1
xeijin <xeijin@users.noreply.github.com>
2018-08-19 13:44:41 +0100

    Remove Scoop env var setting via user

--- script/propositum-post-install.ps1
+++ script/propositum-post-install.ps1
@@ -8,4 +8,19 @@ Catch
     $error[0]|format-list -force
 }
 $otherVars | Select "exec", "var", "value" | ForEach-Object { if ($_.exec -eq "execute") {New-Variable $_.var (iex $_.value) -Force} else {New-Variable $_.var $_.value -Force}}
-[environment]::setEnvironmentVariable('SCOOP',($propositum.root),'User')
+#[environment]::setEnvironmentVariable('SCOOP',($propositum.root),'User')
+$propositumScoop = @(
+    'cmder',
+    'lunacy',
+    'autohotkey',
+    'miniconda3',
+    'imagemagick',
+    'knime-p',
+    'rawgraphs-p',
+    'regfont-p',
+    'emacs-p',
+    'texteditoranywhere-p',
+    'superset-p',
+    'pandoc'
+) 
+$propositumScoop | % { iex "scoop reset $_" }
--- script/propositum-windows.ps1
+++ script/propositum-windows.ps1
@@ -21,10 +21,51 @@ elseif ($testing) {"testing"} # For debugging locally
 elseif ($env:computername -match "NDS.*") {"local-gs"} # Check for a GS NDS
 else {"local"}
 
-cd $PSScriptRoot
-if (-not $env:APPVEYOR) {. ./propositum-prepare-init.ps1}
+$buildPlatform
 
-[environment]::setEnvironmentVariable('SCOOP',($propositum.root),'User')
+$buildPlatform
+
+cd $PSScriptRoot
+
+$Host.UI.RawUI.BackgroundColor = ($bckgrnd = 'Black')
+
+. ./propositum-helper-fns.ps1
+
+Try
+{
+    $environmentVars = Import-CSV "vars-platform.csv"
+}
+Catch
+{
+    Throw "Check the CSV file actually exists and is formatted correctly before proceeding."
+    $error[0]|format-list -force
+}
+
+$environmentVars | Select "exec", "var", $buildPlatform | ForEach-Object { if ($_.exec -eq "execute") {New-Variable $_.var (iex $_.$buildPlatform) -Force} else {New-Variable $_.var $_.$buildPlatform -Force}}
+
+Try
+{
+    $otherVars = Import-CSV "vars-other.csv"
+}
+Catch
+{
+    Throw "Check the CSV file actually exists and is formatted correctly before proceeding."
+    $error[0]|format-list -force
+}
+
+$otherVars | Select "exec", "var", "value" | ForEach-Object { if ($_.exec -eq "execute") {New-Variable $_.var (iex $_.value) -Force} else {New-Variable $_.var $_.value -Force}}
+
+if ($testing -and $propositumLocation) {Remove-Item ($propositumLocation+"\*") -Recurse -Force}
+
+subst $drv $propositumLocation
+
+$createdDirs = Path-CheckOrCreate -Paths $propositum.values -CreateDir
+
+cd $propositum.root
+
+[Net.ServicePointManager]::SecurityProtocol = "Tls12, Tls11, Tls, Ssl3"
+
+#[environment]::setEnvironmentVariable('SCOOP',($propositum.root),'User')
 
 iex (new-object net.webclient).downloadstring('https://get.scoop.sh')
 
