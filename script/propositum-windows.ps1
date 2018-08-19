c9374018ff02cbeaba87cf0d82e2db9dd13f8ee0
xeijin <xeijin@xeijin>
2018-08-19 21:42:34 +0100

    Fix variable typo

--- script/propositum-windows.ps1
+++ script/propositum-windows.ps1
@@ -75,8 +75,6 @@ ForEach ($var in $otherVars) {
     }
 }
 
-Throw "Break for Debug"
-
   if ($testing -and $propositumLocation) {Remove-Item ($propositumLocation+"\*") -Recurse -Force}
 
     subst $env:propositumDrv $env:propositumLocation
