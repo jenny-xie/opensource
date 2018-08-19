cd4ebe86722885f39241cf4f7615a368d293dd93
xeijin <xeijin@xeijin>
2018-08-19 20:03:38 +0100

    Enhance Finish script & Documentation clean-up

--- script/propositum-post-install.ps1
+++ script/propositum-post-install.ps1
@@ -54,6 +54,7 @@ ForEach ($var in $otherVars) {
 }
   #[environment]::setEnvironmentVariable('SCOOP',($propositum.root),'User')
     subst $drv $propositumLocation
+reg add HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Run /f /v "Propositum" /d "subst $propositumDrv $propositumLocation" # Add registry entry to map on startup
 $propositumScoop = @(
     'cmder',
     'lunacy',
