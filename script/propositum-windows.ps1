a461c26d4d2b83a17bff3c1aa03b338bd38cfa96
xeijin <dev@xeijin.org>
2019-03-03 16:19:26 +0000

    Add fuxialexander's profile to properly test whether git-bash approach is working as intended.

--- script/propositum-windows.ps1
+++ script/propositum-windows.ps1
@@ -82,6 +82,8 @@ $propositum | Format-Table | Out-String | Write-Host
   scoop bucket add extras
 
   scoop bucket add propositum 'https://gitlab.com/xeijin-dev/propositum-bucket.git'
+  
+  git clone https://github.com/fuxialexander/doom-emacs-private-xfu %USERPROFILE%/.doom.d
 
   $propositumComponents = @(
       'doom-emacs-p'
